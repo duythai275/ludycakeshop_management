f1f1b1bed1ae4b56390062623be3db5e7e37a573
Thai Nguyen <808278@ACDM.DS.SAIT.CA>
2020-06-11 09:55:39 -0600

    Integrate Electron to Create-React-App

---
+++ public/Main.js
@@ -0,0 +1,18 @@
+const {  app, BrowserWindow } = require('electron');
+const path = require('path');
+
+const createWindow = () => {   
+    // Create the browser window.     
+    win = new BrowserWindow({width: 800, height: 600});
+       
+    // and load the index.html of the app.     
+    // win.loadFile('index.html');
+
+    // load the url
+    // win.loadURL("http:/localhost:3000/");
+    win.loadURL(`file://${path.join(__dirname, '../build/index.html')}`)
+
+    // win.webContents.openDevTools()
+}      
+
+app.on('ready', createWindow);
